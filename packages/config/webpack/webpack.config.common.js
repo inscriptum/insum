607401f16dd247c2a48a9b78cb9ec73905bd2e95
sumbad <sumbad@ya.ru>
2018-10-02 00:31:57 +0800

    add babel config

--- packages/config/webpack/webpack.config.common.js
+++ packages/config/webpack/webpack.config.common.js
@@ -3,7 +3,7 @@
 
 
 
-module.exports = function(helper) {
+module.exports = function (helper) {
   return {
     mode: process.env.NODE_ENV,
     output: {
@@ -13,59 +13,63 @@ module.exports = function(helper) {
     },
     module: {
       rules: [{
-          test: /\.ts$/,
-          use: [
-            {
-              loader: "babel-loader",
-              options: {
-                babelrc: process.env.NODE_ENV === 'production'
+        test: /\.ts$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: process.env.NODE_ENV === 'production'
+              ? {
+                presets: ['@babel/preset-env'],
+                // support dynamic import syntax, but leave it unchanged
+                plugins: [require('@babel/plugin-syntax-dynamic-import')]
               }
-            },
-            {
-              loader: "ts-loader"
-            }
-          ],
-          exclude: /node_modules/
-        },
-        {
-          test: /\.js$/,
-          loader: 'babel-loader',
-          exclude: [helper.PATHS.node_modules]
-        },
-        {
-          test: /\.(png|jpg|jpeg|gif|svg)(\?v=\d+\.\d+\.\d+)?$/,
-          loader: 'file-loader',
-          options: {
-            name: '[folder]/[name].[ext]?[hash]'
+              : {}
+          },
+          {
+            loader: "ts-loader"
           }
-        },
-        {
-          test: /\.(eot|woff|woff2|ttf)(\?v=\d+\.\d+\.\d+)?$/,
-          use: 'file-loader?name=[folder]/[name].[ext]',
-        },
-        {
-          test: /\.scss$/,
-          use: [{
-              loader: "raw-loader"
-            },
-            {
-              loader: 'postcss-loader',
-            },
-            {
-              loader: "sass-loader"
-            }
-          ],
-        },
-        {
-          test: /\.css$/,
-          use: [{
-            loader: 'raw-loader'
-          }, ],
-        },
-        {
-          test: /\.html$/,
-          use: ['html-loader'],
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: [helper.PATHS.node_modules]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+        options: {
+          name: '[folder]/[name].[ext]?[hash]'
         }
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf)(\?v=\d+\.\d+\.\d+)?$/,
+        use: 'file-loader?name=[folder]/[name].[ext]',
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "raw-loader"
+        },
+        {
+          loader: 'postcss-loader',
+        },
+        {
+          loader: "sass-loader"
+        }
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [{
+          loader: 'raw-loader'
+        },],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      }
       ]
     },
     resolve: {
