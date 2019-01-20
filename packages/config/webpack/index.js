631c95ebefa1d3935bcb725b271dea0336a011d4
Den Sumbaev <d.sumbaev@ispsystem.com>
2019-01-21 06:59:19 +0800

    create modules for webpack config

---
+++ packages/config/webpack/index.js
@@ -0,0 +1,55 @@
+module.exports = {
+  module_rule: {
+    typescript: {
+      test: /\.ts$/,
+      use: () => {
+        let loaders = [];
+        if (process.env.NODE_ENV === 'production') {
+          loaders.push("babel-loader");
+        }
+        loaders.push("ts-loader");
+        return loaders
+      },
+      exclude: /node_modules/
+    },
+    javascript: {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: [helper.PATHS.node_modules]
+    },
+    image: {
+      test: /\.(png|jpg|jpeg|gif|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'file-loader',
+      options: {
+        name: '[folder]/[name].[ext]?[hash]'
+      }
+    },
+    font: {
+      test: /\.(eot|woff|woff2|ttf)(\?v=\d+\.\d+\.\d+)?$/,
+      use: 'file-loader?name=[folder]/[name].[ext]',
+    },
+    scss_raw: {
+      test: /\.scss$/,
+      use: [{
+        loader: "raw-loader"
+      },
+      {
+        loader: 'postcss-loader',
+      },
+      {
+        loader: "sass-loader"
+      }
+      ],
+    },
+    css_raw: {
+      test: /\.css$/,
+      use: [{
+        loader: 'raw-loader'
+      },],
+    },
+    html: {
+      test: /\.html$/,
+      use: ['html-loader'],
+    }
+  }
+}
