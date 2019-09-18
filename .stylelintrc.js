fd11177fe7a0b94a2262f142dcdc0a28444272fa
Den Sumbaev <d.sumbaev@ispsystem.com>
2019-09-18 23:04:00 +0800

    refactor: 💡 update the monoproject structure and settings

---
+++ .stylelintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "plugins": [
+    "stylelint-scss",
+    "stylelint-order"
+  ],
+  "extends": [
+    "stylelint-config-recommended-scss",
+    "stylelint-config-htmlacademy"
+  ],
+  "rules": {
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": true,
+  },
+};
