427ef7ad32b4c8096d4e1440d2f4b51cf55a2b6c
Den Sumbaev <d.sumbaev@ispsystem.com>
2019-09-18 23:01:33 +0800

    feat(babel.config): 🌟 create config
    BREAKING CHANGE: @insum/config deprecated, use one of new config

---
+++ packages/configs/babel/babel.config.js
@@ -0,0 +1,28 @@
+module.exports = function (api) {
+  // cache if funciton result didn't change
+  api.cache(() => process.env.NODE_ENV);
+
+  const presets = [
+    [
+      "@babel/preset-env",
+      {
+        // leave imports as they are
+        "modules": false,
+        "targets": {
+          "esmodules": true
+        },
+        // adds specific imports for polyfills when they are used in each file
+        "useBuiltIns": 'usage',
+      }
+    ]
+  ];
+
+  const plugins = [
+    "@babel/plugin-syntax-dynamic-import"
+  ];
+
+  return {
+    presets,
+    plugins
+  };
+}
