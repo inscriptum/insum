b80bbbd3dd923207c9500733204ff88eea728480
sumbad <sumbad@ya.ru>
2019-11-07 16:19:24 +0800

    feat(webpack.config): 🌟 individual webpack loaders
    Every webpack loader install its dependencies itself
    
    BREAKING CHANGE: To use a loader need to import it separately

--- packages/configs/webpack/_installer.js
+++ packages/configs/webpack/_installer.js
@@ -5,18 +5,17 @@ function installDependencies(dependencies, autoInstall = true) {
     if (Array.isArray(dependency)) {
       const orDeps = installDependencies(dependency, false);
       if (orDeps.some(d => d)) {
-        execSync(`npm i ${dependency[0]} -D && npm link @insum/webpack.config`, { stdio: 'inherit', shell: true });
+        execSync(`npm i ${dependency[0]} -D`, { stdio: 'inherit', shell: true });
         return true;
       }
     } else {
       try {
-        console.log(111, require(dependency).version);
+        // eslint-disable-next-line no-console
+        console.log(`${dependency} version ${require(dependency).version}`);
         return true;
       } catch (error) {
-        console.log(222, error);
-        
         if (autoInstall) {
-          execSync(`npm i ${dependency} -D && npm link @insum/webpack.config`, { stdio: 'inherit', shell: true });
+          execSync(`npm i ${dependency} -D`, { stdio: 'inherit', shell: true });
           return true;
         } else {
           return false;
