a3342b57ebd1c858df0aa486db5605e782322950
sumbad <sumbad@ya.ru>
2019-11-08 00:45:22 +0800

    refactor(webpack.config): 💡 install one of dependencies for a loader

--- packages/configs/webpack/_installer.js
+++ packages/configs/webpack/_installer.js
@@ -4,7 +4,7 @@ function installDependencies(dependencies, autoInstall = true) {
   return dependencies.map(dependency => {
     if (Array.isArray(dependency)) {
       const orDeps = installDependencies(dependency, false);
-      if (orDeps.some(d => d)) {
+      if (!orDeps.some(d => d)) {
         // eslint-disable-next-line no-console
         console.log(`Try to install ${dependency[0]}`);
         execSync(`npm i ${dependency[0]} -D`, { stdio: 'inherit', shell: true });
