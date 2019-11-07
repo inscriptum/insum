bee20070194d65487a37a3bde28164715e20fd24
sumbad <sumbad@ya.ru>
2019-11-08 00:00:14 +0800

    refactor(webpack.config): 💡 add more information before install dependencies

--- packages/configs/webpack/_installer.js
+++ packages/configs/webpack/_installer.js
@@ -5,16 +5,20 @@ function installDependencies(dependencies, autoInstall = true) {
     if (Array.isArray(dependency)) {
       const orDeps = installDependencies(dependency, false);
       if (orDeps.some(d => d)) {
+        // eslint-disable-next-line no-console
+        console.log(`Try to install ${dependency[0]}`);
         execSync(`npm i ${dependency[0]} -D`, { stdio: 'inherit', shell: true });
         return true;
       }
     } else {
       try {
         // eslint-disable-next-line no-console
-        console.log(`${dependency} version ${require(dependency).version}`);
+        console.log(`${dependency} version ${require(`${dependency}/package.json`).version}`);
         return true;
       } catch (error) {
         if (autoInstall) {
+          // eslint-disable-next-line no-console
+          console.log(`Try to install ${dependency}`);
           execSync(`npm i ${dependency} -D`, { stdio: 'inherit', shell: true });
           return true;
         } else {
