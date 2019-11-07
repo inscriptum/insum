bbbe96fcbcbab4a8b56e17c492b25f9d4ea22ad0
sumbad <sumbad@ya.ru>
2019-11-07 16:10:35 +0800

    refactor(webpack.config): 💡 remove loaders from common config

--- packages/configs/webpack/webpack.config.common.js
+++ packages/configs/webpack/webpack.config.common.js
@@ -6,64 +6,6 @@ module.exports = function(helper) {
       publicPath: '/',
       filename: '[name].js',
     },
-    module: {
-      rules: [
-        {
-          test: /\.ts$/,
-          use: () => {
-            const loaders = [];
-            if (process.env.NODE_ENV === 'production') {
-              loaders.push('babel-loader');
-            }
-            loaders.push('ts-loader');
-            return loaders;
-          },
-          exclude: /node_modules/,
-        },
-        {
-          test: /\.js$/,
-          loader: 'babel-loader',
-          exclude: [helper.PATHS.node_modules],
-        },
-        {
-          test: /\.(png|jpg|jpeg|gif|svg)(\?v=\d+\.\d+\.\d+)?$/,
-          loader: 'file-loader',
-          options: {
-            name: '[folder]/[name].[ext]?[hash]',
-          },
-        },
-        {
-          test: /\.(eot|woff|woff2|ttf)(\?v=\d+\.\d+\.\d+)?$/,
-          use: 'file-loader?name=[folder]/[name].[ext]',
-        },
-        {
-          test: /\.scss$/,
-          use: [
-            {
-              loader: 'raw-loader',
-            },
-            {
-              loader: 'postcss-loader',
-            },
-            {
-              loader: 'sass-loader',
-            },
-          ],
-        },
-        {
-          test: /\.css$/,
-          use: [
-            {
-              loader: 'raw-loader',
-            },
-          ],
-        },
-        {
-          test: /\.html$/,
-          use: ['html-loader'],
-        },
-      ],
-    },
     resolve: {
       modules: [helper.PATHS.src, helper.PATHS.node_modules],
       extensions: ['.ts', '.js', '.json'],
