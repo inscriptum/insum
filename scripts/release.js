30799be3ea10429743db05f41b64260b75e6a37f
Denis Sumbaev <denis.sumbaev@gmail.com>
2019-12-06 17:31:11 +0800

    chore: 🔧 add flag --if-present to run npm scripts 'test' & 'build'

--- scripts/release.js
+++ scripts/release.js
@@ -7,7 +7,7 @@ execSync('npx lerna bootstrap', { stdio: 'inherit', shell: true });
 // Testing and building packages before publish
 const changedPackages = JSON.parse(execSync('npx lerna changed --include-merged-tags --json', { encoding: 'utf8' }));
 const packagesScope = changedPackages.map(p => `--scope="${p.name}"`).join(' ');
-const testPackagesCmd = `npx lerna exec --stream ${packagesScope} "npm test && npm run build"`;
+const testPackagesCmd = `npx lerna exec --stream ${packagesScope} "npm test --if-present && npm run build --if-present"`;
 console.log(`> ${testPackagesCmd}`);
 execSync(testPackagesCmd, { stdio: 'inherit', shell: true });
 
