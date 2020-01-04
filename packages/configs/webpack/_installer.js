fc05d32b15d98d9be9e9bed7e8b5d345eae597d4
Denis Sumbaev <denis.sumbaev@gmail.com>
2020-01-04 15:16:26 +0800

    refactor(webpack.config): 💡 create custom logger object

--- packages/configs/webpack/_installer.js
+++ packages/configs/webpack/_installer.js
@@ -1,24 +1,34 @@
 const { execSync } = require('child_process');
 
+const logger = {
+  info: function(...args) {
+    if (process.env.NODE_ENV !== 'production') {
+      // eslint-disable-next-line no-console
+      console.info.apply(this, args);
+    }
+  },
+  warn: function(...args) {
+    console.warn.apply(this, args);
+  },
+};
+
 function installDependencies(dependencies, autoInstall = true) {
   return dependencies.map(dependency => {
     if (Array.isArray(dependency)) {
       const orDeps = installDependencies(dependency, false);
       if (!orDeps.some(d => d)) {
-        // eslint-disable-next-line no-console
-        console.log(`Try to install ${dependency[0]}`);
+        logger.warn(`Try to install ${dependency[0]}`);
         execSync(`npm i ${dependency[0]} -D`, { stdio: 'inherit', shell: true });
         return true;
       }
     } else {
       try {
-        // eslint-disable-next-line no-console
-        console.log(`${dependency} version ${require(`${dependency}/package.json`).version}`);
+        const version = require(`${dependency}/package.json`).version;
+        logger.info(`${dependency} version ${version}`);
         return true;
       } catch (error) {
         if (autoInstall) {
-          // eslint-disable-next-line no-console
-          console.log(`Try to install ${dependency}`);
+          logger.warn(`Try to install ${dependency}`);
           execSync(`npm i ${dependency} -D`, { stdio: 'inherit', shell: true });
           return true;
         } else {
